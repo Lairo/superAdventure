0b4e419e7038d73ac06432605666ab0972128ba9
Lairo <chajus1@googlemail.com>
2018-11-26 04:21:25 +0000

    Added Basic inheritance

--- Engine/LivingCreature.cs
+++ Engine/LivingCreature.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace Engine
 {
-    public class Weapon : Item
-    {        
-        public int MaximumDamage { get; set; }
-        public int MinimumDamage { get; set; }
+    public class LivingCreature
+    {
+        public int CurrentHitPoints { get; set; }
+        public int MaximumHitPoints { get; set; }
     }
 }
