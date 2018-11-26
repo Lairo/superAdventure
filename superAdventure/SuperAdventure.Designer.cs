c15e22e0c10dc3b9e0e38c2ef949f62280fe9bca
Lairo <chajus1@googlemail.com>
2018-11-26 02:02:38 +0000

    Removed test button. Created player class and player object. Created additional classes.

--- superAdventure/SuperAdventure.Designer.cs
+++ superAdventure/SuperAdventure.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace superAdventure
 {
-    partial class Form1
+    partial class SuperAdventure
     {
         /// <summary>
         /// Required designer variable.
@@ -36,7 +36,6 @@
             this.lblGold = new System.Windows.Forms.Label();
             this.lblExperience = new System.Windows.Forms.Label();
             this.lblLevel = new System.Windows.Forms.Label();
-            this.btnTest = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -113,23 +112,11 @@
             this.lblLevel.TabIndex = 7;
             this.lblLevel.Click += new System.EventHandler(this.label8_Click);
             // 
-            // btnTest
-            // 
-            this.btnTest.AccessibleName = "";
-            this.btnTest.Location = new System.Drawing.Point(220, 172);
-            this.btnTest.Name = "btnTest";
-            this.btnTest.Size = new System.Drawing.Size(75, 23);
-            this.btnTest.TabIndex = 8;
-            this.btnTest.Text = "Test";
-            this.btnTest.UseVisualStyleBackColor = true;
-            this.btnTest.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // Form1
+            // SuperAdventure
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(719, 651);
-            this.Controls.Add(this.btnTest);
             this.Controls.Add(this.lblLevel);
             this.Controls.Add(this.lblExperience);
             this.Controls.Add(this.lblGold);
@@ -138,7 +125,7 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Name = "Form1";
+            this.Name = "SuperAdventure";
             this.Text = "My Game";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
@@ -156,7 +143,6 @@
         private System.Windows.Forms.Label lblGold;
         private System.Windows.Forms.Label lblExperience;
         private System.Windows.Forms.Label lblLevel;
-        private System.Windows.Forms.Button btnTest;
     }
 }
 
--- superAdventure/SuperAdventure.cs
+++ superAdventure/SuperAdventure.cs
@@ -8,24 +8,37 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using Engine;
+
 namespace superAdventure
 {
-    public partial class Form1 : Form
+    public partial class SuperAdventure : Form
     {
-        public Form1()
+        private Player _player;
+
+        public SuperAdventure()
         {
             InitializeComponent();
+
+            _player = new Player();
+
+            _player.CurrentHitPoints = 10;
+            _player.MaximumHitPoints = 10;
+            _player.Gold = 20;
+            _player.ExperiencePoints = 0;
+            _player.Level = 1;
+
+            lblHitPoints.Text = _player.CurrentHitPoints.ToString();
+            lblGold.Text = _player.Gold.ToString();
+            lblExperience.Text = _player.ExperiencePoints.ToString();
+            lblLevel.Text = _player.Level.ToString();
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            lblGold.Text = "123";
-        }
+        }        
 
         private void label8_Click(object sender, EventArgs e)
         {
