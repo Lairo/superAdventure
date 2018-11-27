8027e0ab122b5bb834e8240494caa3dacf618739
Lairo <chajus1@googlemail.com>
2018-11-27 00:05:41 +0000

    Added InventoryItem, LootItem, PlayerQuest, QuestCompletetionItem classes and moded existing classes to utilise Lists

---
+++ Engine/InventoryItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Engine;
+
+namespace Engine
+{
+    public class InventoryItem
+    {
+        public Item Details { get; set; }
+        public int Quantity { set; get; }
+
+        public InventoryItem(Item details, int quantity)
+        {
+            Details = details;
+            Quantity = quantity;
+        }
+    }
+}
--- Engine/Player.cs
+++ Engine/Player.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Engine
 {
@@ -9,6 +11,8 @@ namespace Engine
         public int Gold { get; set; }
         public int ExperiencePoints { get; set; }
         public int Level { get; set; }
+        public List<InventoryItem> Inventory { get; set; }
+        public List<PlayerQuest> Quests { get; set; }
 
         public Player(int currentHitPoints, int maximumHitPoints,
             int gold, int experiencepoints, int level) : 
@@ -17,6 +21,8 @@ namespace Engine
             Gold = gold;
             ExperiencePoints = experiencepoints;
             Level = level;
+            Inventory = new List<InventoryItem>();
+            Quests = new List<PlayerQuest>();
         }
     }
 }
